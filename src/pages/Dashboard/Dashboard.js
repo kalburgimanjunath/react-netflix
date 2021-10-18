2cfeab0bb295a44e7fd8bc621d681a2fd4f37f94
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-19 00:42:13 +0530

    3 files modified

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,14 @@
 import React from 'react';
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+import Card from '../Card';
+export default function Dashboard({ title }) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <Card title="Top Pics of ----" />
+      <Card title="Trending Now" />
+      <Card title="You Watched recently" />
+      <Card title="New Releases" />
+      <Card title="Genres" />
+    </>
+  );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
-import { Login, Home, Signup } from './index';
+import { Login, Home, Signup, Dashboard } from './index';
 export default function Main() {
   return (
     <div>
@@ -10,6 +10,13 @@ export default function Main() {
         </Route>
         <Route path="/home">
           <Home title="Home" />
+        </Route>
+
+        <Route path="/signup">
+          <Signup title="Signup" />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard title="Dashboard" />
         </Route>
         <Route path="/">
           <Home title="Home" />
