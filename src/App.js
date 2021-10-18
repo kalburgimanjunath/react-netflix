fee099cd84e114d6a0155e432d6f2bfafbc1c4b6
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-18 23:18:23 +0530

    2 files modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './style.css';
-
+import { Header } from './components/index';
+import { BrouserRouter } from 'react-router-dom';
 export default function App() {
   return (
     <div>
-      <Header />
+      <BrouserRouter>
+        <Header />
+      </BrouserRouter>
     </div>
   );
 }
---
+++ src/components/sample.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function sample(){
+  return(){
+    <div></div>
+  }
+}
